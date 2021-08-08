bf88228f9f4f5ad2b1ca77fb0bae3ebf51e767e8
venkata nihar billakurthi <84034294+venkataniharbillakurthi@users.noreply.github.com>
2021-08-08 15:19:22 +0530

    Create set 10 ( b_area_perimeter_ )rectangle.c

---
+++ set 10 ( b_area_perimeter_ )rectangle.c
@@ -0,0 +1,11 @@
+// 10 b) write a c program to compute area and perimeter of rectangle
+#include<stdio.h>
+void main()
+{
+	int h, w, a, p;
+	h=7;
+	w=5;
+	a= h*w;
+	p= 2 * (h+w);
+	printf("\nArea of the rectange is %d \nPerimeter of the rectangle is %d", a, p);
+}
